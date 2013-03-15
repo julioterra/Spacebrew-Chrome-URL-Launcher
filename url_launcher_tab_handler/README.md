fd039fd571b1b863dd65c1709c6813c0b04d12cf
julio terra <julioterra@gmail.com>
2013-03-15 02:12:32 -0400

    created a new version of the url launcher for spacebrew. Moved to my personal repo since I am know working on maintaining this project outside of the LAB.

---
+++ url_launcher_tab_handler/README.md
@@ -0,0 +1,59 @@
+Spacebrew URL Launcher
+======================
+
+This chrome extension is the spacebrew Url Launcher app. This app accepts a string message from spacebrew, checks if that message is a URL, and if so, loads the URL in Chrome. This app can also function as a tab manager that always keeps a chrome tab in focus, and deletes all inactive tabs. Please read the instructions below to learn how to use it.
+
+App: 		spacrebrew_url_launcher
+Component: 	url_launcher chrome extension
+Team: 		Julio Terra
+Date: 		March 15, 2013
+
+System Requirements:
+* Mac OSX (may work on other platforms but has not been tested)
+* Chrome
+
+How to Use 
+============================  
+
+Setting Up the URL Launcher:
+----------------------------  
+1. Install the Chrome Extension on the Chrome browser that will be running this app by following these steps:
+	1. launch the instance of Chrome where this app will run
+	2. navigate to chrome://extensions
+	3. check "Developer" (top right)
+	4. click on the load unpackaged extension button and navigate the root directory of the url launcher
+	5. click "Accept"
+2. Navigate to any URL in Chrome and include all of the appropriate query string options in your URL. Please refer to the section query string option descriptions below.
+3. Go to the admin page for the Spacebrew server that your app is linked to and confirm that the app is showing up. Then link your app to an app that send urls.
+
+Query String Options:
+---------------------  
+The following option flags (true/false) can be set via the query string to change behavior of this chrome extension:
+
+* active: when set to "true" it identifies the current tab as the active tab, and proceeds to read all query string options. the url launcher ads the query string "active=true" to all urls that it launches. 
+* url_launcher: if url_launcher is set to "true" then chrome extension will function as a url launcher and will connect to interactive spaces. Defaults to "false".
+* fullscreen: when set to "true" the url_launcher automatically goes to fullscreen and attempt to stay in fullscreen at all times. Defaults to "false".
+* keep_tabs: when "keep_tabs" is set to "false" the url_launcher deletes all tabs other than the active tab. It also attempts to make sure that the Chrome app is always in focus. Defaults to "true".
+* timeout: time in milliseconds used to set time limit for an idle timer. When a web page is idle the url_launcher reloads the active page and sends a true boolean message to Spacebrew via the "im_bored" outlet. Defaults to 0.
+* debug: when set to "true" turns on js console messages. Defauls to "false".
+
+The following options configure the connection to Spacebrew. You don't need to specify these items if you want to connect to the server using the default name, and if you are connecting to the cloud server at 'sandbox.spacebrew.cc':
+* name: holds the name for this application that will show up on the spacebrew admin interface. Defaults to "sbUrlLauncher".
+* server: holds the hostname of the spacebrew server to which this application should connect. Defaults to "localhost".
+* port: holds the port number that is being used by the spacebrew server. This is an optional param. Defaults to 9000. 
+
+Query String Samples:
+---------------------  
+Here is are a few query string samples, along with what they mean:
+
+1. Query string that starts up the URL launcher by settings url_launcher=true, sets the current tab to active by setting active=true. This only works if you are using a spacebrew server that is running on your localhost, as this is the default behavior when a server is not specified in the query string. 
+```
+[ANY URL]?url_launcher=true&active=true&
+```
+	
+2. Query string that starts up the URL launcher by settings url_launcher=true, sets the current tab to active by setting active=true, sets the hostname of the Spacebrew servers by setting server=serverName, tells the app NOT to go to fullscreen with go_fullscreen=false, and sets keep_tabs=true so that the app will keep inactive tabs open and let users navigate away from the active tab.
+```
+[ANY URL]?url_launcher=true&active=true&server=serverName&go_fullscreen=false&keep_tabs=true&
+```
+
+**Important Note:** It is good to add the trailing "&" to the query string because if you are loading a site at a directory level (there is no file extension at the end of the url) a forward slash is sometimes appended to the last query element.
---
+++ url_launcher_tab_handler/js/content_script.js
@@ -0,0 +1,147 @@
+/*!
+ *  URL Launcher :: Content Script  *
+ * 
+ * 	A javascript file that is injected into all web pages by the URL Launcher.
+ * 	It reads the query string options and passes this information to the background
+ * 	process, which responds to confirm that the connection was established. This
+ * 	code also sets up an idle timer, when the response from the background process
+ * 	includes an "idle" attribute with an integer.
+ * 	
+ * @filename    content_script.js
+ * @author      Julio Terra
+ * @modified    3/15/2013
+ * @version     3.0.1
+ * 
+ */
+
+var options = {};
+var connection = false;
+var idle_timeout = 0;
+var debug = true;
+
+$(document).ready(function() {
+	console.log("[urlLauncher:page loaded] ");
+	readQueryStringOptions();
+});
+
+/**
+ * readQueryStringOptions method that parses the query string and saves relevant data into the options variable.
+ *  	It then connects to the chrome extension background app and sends it the query string data. It sets a timeout
+ *   	that attempts to reconnect to the background app if the connection was not established successfuly.
+ * @return {none} 
+ */
+function readQueryStringOptions() {
+	options.href = window.location.href;
+
+	options.active = getQueryString("active") == "true" ? true : false; 
+	options.url_launcher = getQueryString("url_launcher") == "true" ? true : false; 
+	options.debug = debug = getQueryString("debug")  == "true" ? true : false; 
+	options.go_fullscreen = getQueryString("fullscreen") == "true" ? true : false;
+	options.keep_tabs = getQueryString("keep_tabs") != "false" ? true : false;
+
+	options.name = getQueryString("name") || false;
+	options.server = getQueryString("server") || false;
+	options.port = getQueryString("port") || 9000;
+
+	options.timeout = getQueryString("timeout") || 0;
+
+	if (options.debug) console.log("[urlLauncher:readQueryStringOptions] sending options: ", options);
+	if (options.debug) addToStatus("sending request to connect to extension");
+	chrome.extension.sendMessage(options, handleResponse);	
+
+	// set timeout for when to check whether connection was established.
+	setTimeout( function() {
+		if (!connection) readQueryStringOptions();
+	}, 4000);
+}
+
+/**
+ * handleResponse callback method that is used to handle response from the background app to the connection request
+ *  	made in readQueryStringOptions(). It checks if the connection was succesfully established by checking 
+ *  	whether the "response" argument is defined. If it is undefined the the connection flag is set to false, 
+ *  	otherwise the conection flag is set to true. If the response includes the attribute "idle" then it will
+ *  	attempt to start-up and idle timer.
+ * @param  {object} response Object that holds response from the background app to messages from this injected
+ *                           script. If the background app was running properly then it will return an empty object
+ *                           otherwise it returns an undefined object.
+ * @return {none}          
+ */
+function handleResponse(response) {
+ 	if (options.debug) console.log("[urlLauncher:handleResponse] received response from from backend ");
+ 	if (options.debug) console.log(response);
+
+ 	if (response == undefined) {
+ 		connection = false;
+		if (options.debug) console.log("[urlLauncher:handleResponse] connection attempt failed, try again in 4 second");
+		if (options.debug) addToStatus("failure - connection to extension did not work ");
+ 	}
+ 	else {
+ 		connection = true;
+		if (options.debug) console.log("[urlLauncher:handleResponse] connection made");
+		if (options.debug) addToStatus("success - connection to extension established");
+
+ 		// set idle timer if the response included the attribute "idle"
+		if (response.idle) {
+			if (options.debug) console.log("[urlLauncher:handleResponse] setting idle timer: " + response.idle); 		
+			startIdleTimer(response.idle);
+		}
+ 	}
+}
+
+/**
+ * startIdleTimer inserts an idle timer into the current tab
+ * @param  {integer} timeout Timeout time for the idel timer in milliseconds.
+ * @return {none}         
+ */
+function startIdleTimer(timeout){
+	if (isNaN(timeout) || timeout <= 0) {
+		if (debug) console.log("[urlLauncher:startIdleTimer] argument is not a number " + timeout);		
+		return;
+	}
+
+	idle_timeout = timeout
+	if (debug) console.log("[urlLauncher:startIdleTimer] loading idle timer");
+	$.idleTimer(parseInt(timeout));
+	$(document).bind("idle.idleTimer", pageIdle);
+};
+
+/**
+ * callback method that is called when the idle timer times out. It sends a message to the
+ *  	background app to let us know that the page is currently idle.
+ * @return {none} 
+ */
+function pageIdle(){
+	if (debug) console.log("[urlLauncher:pageIdle] page has been idle for " + idle_timeout);
+	chrome.extension.sendMessage({idle: true, timeout: idle_timeout}, function(response) {});		
+};
+
+/**
+ * method that handles the status messages that appear on the browser window while this content script
+ * 		is attempting to connect to the background app. Every time it logs a new message it adds a timestamp. It 
+ *   	then displays the message in the #extensionStatus div
+ * 
+ * @param {string} string Holds the latest message update to add to the status messages that appear on screen
+ */
+var addToStatus = function(string) {
+	if (!document.querySelector("#extensionStatus")) return;
+	var today=new Date();
+	var now = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	var curStatus = document.querySelector("#extensionStatus").innerText;
+	document.querySelector("#extensionStatus").innerText = curStatus + now + " - " + string  + " \\\r ";	
+};
+
+/**
+ * method that parses arguments from the query string
+ * @param  {String} name 	The key of the query string key/value pair that will be parsed 
+ * @return {String}      	The value associated to the key/value pair being parsed
+ */
+var getQueryString = function( name ) {
+	if (!window.location) return;
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( window.location.href );
+	if( results == null ) return "";
+	else return results[1];
+}
+
